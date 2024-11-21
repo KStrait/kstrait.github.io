b6e1bfa6869c0e72a37b7edf27eb24ad0ba66863
KStrait <KyleL.Strait@gmail.com>
2024-11-21 14:44:31 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,8 @@
   // Wait for the document to load before running the script
   $(window).on('load hashchange', function() {
     
-    // First, hide all content regions by removing the 'show' class
-    $('.content-region').removeClass('show');
+    // First, remove both 'show' and 'hide' classes from all content regions
+    $('.content-region').removeClass('show').addClass('hide');
     
     // Remove any active classes on the main-menu
     $('.main-menu a').removeClass('active');
@@ -12,8 +12,8 @@
     // If a hash is present in the URL, use it, otherwise default to the first menu item
     var region = location.hash.toString() || '#home'; // Default to '#home' if no hash is present
     
-    // Show the region specified in the URL hash by adding the 'show' class
-    $(region).addClass('show');
+    // Show the region specified in the URL hash by adding the 'show' class and removing the 'hide' class
+    $(region).removeClass('hide').addClass('show');
     
     // Highlight the menu link associated with this region by adding the .active CSS class
     $('.main-menu a[href="'+ region +'"]').addClass('active');
