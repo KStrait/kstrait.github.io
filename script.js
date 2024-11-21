5a737c704008899b14d23c39bb2b58be69c09a3d
KStrait <KyleL.Strait@gmail.com>
2024-11-21 14:20:39 -0500

    Update script.js
    Update js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
   
   // We use some Javascript and the URL #fragment to hide/show different parts of the page
   // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/a#Linking_to_an_element_on_the_same_page
-  $(window).on('load hashchange', function(){
+  $(window).on('load hashchange', function() {
     
     // First hide all content regions, then show the content-region specified in the URL hash 
     // (or if no hash URL is found, default to first menu item)
@@ -11,19 +11,34 @@
     
     // Remove any active classes on the main-menu
     $('.main-menu a').removeClass('active');
-    var region = location.hash.toString() || $('.main-menu a:first').attr('href');
     
-    // Now show the region specified in the URL hash
+    // If a hash is present in the URL, use it, otherwise default to the first menu item
+    var region = location.hash.toString() || '#home'; // Default to '#home' if no hash is present
+    
+    // Show the region specified in the URL hash
     $(region).show();
     
     // Highlight the menu link associated with this region by adding the .active CSS class
-    $('.main-menu a[href="'+ region +'"]').addClass('active'); 
-
-    // Alternate method: Use AJAX to load the contents of an external file into a div based on URL fragment
-    // This will extract the region name from URL hash, and then load [region].html into the main #content div
-    // var region = location.hash.toString() || '#first';
-    // $('#content').load(region.slice(1) + '.html')
-    
+    $('.main-menu a[href="'+ region +'"]').addClass('active');
   });
-  
+
+  // Add event listeners to the navigation links
+  $('#home-link').on('click', function (e) {
+    e.preventDefault();
+    // Change the URL hash and trigger hashchange
+    window.location.hash = 'home';
+  });
+
+  $('#about-link').on('click', function (e) {
+    e.preventDefault();
+    // Change the URL hash and trigger hashchange
+    window.location.hash = 'about';
+  });
+
+  $('#contact-link').on('click', function (e) {
+    e.preventDefault();
+    // Change the URL hash and trigger hashchange
+    window.location.hash = 'contact';
+  });
+
 })(jQuery);
