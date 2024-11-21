270ee887c4a6419ffabfdfaa1b6d0bc3915e883b
KStrait <KyleL.Strait@gmail.com>
2024-11-21 14:41:25 -0500

    Update script.js
    Fixing hide/show

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-// Wait for the document to load before running the script 
 (function ($) {
   
-  // We use some Javascript and the URL #fragment to hide/show different parts of the page
-  // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/a#Linking_to_an_element_on_the_same_page
+  // Wait for the document to load before running the script
   $(window).on('load hashchange', function() {
     
-    // First hide all content regions, then show the content-region specified in the URL hash 
-    // (or if no hash URL is found, default to first menu item)
-    $('.content-region').hide();
+    // First, hide all content regions by removing the 'show' class
+    $('.content-region').removeClass('show');
     
     // Remove any active classes on the main-menu
     $('.main-menu a').removeClass('active');
@@ -15,8 +12,8 @@
     // If a hash is present in the URL, use it, otherwise default to the first menu item
     var region = location.hash.toString() || '#home'; // Default to '#home' if no hash is present
     
-    // Show the region specified in the URL hash
-    $(region).show();
+    // Show the region specified in the URL hash by adding the 'show' class
+    $(region).addClass('show');
     
     // Highlight the menu link associated with this region by adding the .active CSS class
     $('.main-menu a[href="'+ region +'"]').addClass('active');
